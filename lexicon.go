c56ae4a28c83089d09c5d9c3edd44fe6ec600813
Douglas Green <bluntsporks+git@gmail.com>
2015-04-26 21:12:52 -0400

    Adding character counter

--- lexicon.go
+++ lexicon.go
@@ -18,6 +18,17 @@ func DefaultDataPath() string {
 	return dir
 }
 
+// CountChars counts all the characters in a language lexicon.
+func CountChars(words map[string]bool) map[rune]int {
+	chars := make(map[rune]int)
+	for word := range words {
+		for _, ch := range word {
+			chars[ch]++
+		}
+	}
+	return chars
+}
+
 // LoadAllLanguages loads all the language files.
 func LoadAllLanguages(langDir string) map[string]map[string]bool {
 	// Load the languages.
